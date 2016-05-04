aeacf78f3678b9b9677c6d493730a4c7112e3f15
Ioritz Cabero <ioritzc@gmail.com>
2016-05-04 22:48:01 +0200

    asd
    as

--- main.py
+++ main.py
@@ -14,25 +14,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 import webapp2
-
-from Estructuras.Usuarios import Usuario
-from Estructuras.Recetas import Receta
-from Estructuras.Pasos import Paso
+import os
+import jinja2
 
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'Templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class Handler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+class MainHandler(Handler):
     def get(self):
-
-        p = Receta.get_by_id(4644337115725824)
-
-        for pa in p.obtener_pasos():
-            self.response.write(pa.descripcion+"<br/>")
-
-
+      self.render("index.html")
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+
